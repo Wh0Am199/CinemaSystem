a9c7dc913dea22109afa073d38979660df5fe3a4
Wh0Am1 <balintb99@gmail.com>
2020-04-06 12:38:38 +0200

    Update a Users tábla kódjában

--- Database/users.sql
+++ Database/users.sql
@@ -1,7 +1,14 @@
 CREATE TABLE `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `address2` varchar(250) DEFAULT NULL,
+  `city` varchar(250) NOT NULL,
+  `region` varchar(250) NOT NULL,
+  `postalcode` varchar(250) NOT NULL,
   `isAdmin` int(1) DEFAULT 0,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
