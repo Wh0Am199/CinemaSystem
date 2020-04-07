7b59984ee29a0275d589a77a0a667e84f5d803e1
Mrk-PTi <markszalai22@gmail.com>
2020-04-07 15:33:07 +0200

    Form kiegészítve a login.php -ban

--- PHP/protected/user/login.php
+++ PHP/protected/user/login.php
@@ -1,17 +1,17 @@
-<form>
+<form method="post">
   <CENTER>
    <div class="form-group row">
-    <label for="inputEmail3" class="col-sm-1 col-form-label" >Email</label>
+    <label for="inputEmail3" class="col-sm-1 col-form-label" >E-mail cím</label>
     <div class="col-sm-3">
-      <input type="email" class="form-control" id="inputEmail3"  placeholder="Email" >
+      <input type="email" class="form-control" id="inputEmail3"  placeholder="teszt.elek@gmail.com" name="email" value="">
     </div>
   </div>
   <div class="form-group row">
     <label for="inputPassword3" class="col-sm-1 col-form-label">Jelszó</label>
     <div class="col-sm-3">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Jelszó">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="********" name="password" value="">
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+  <button type="submit" class="btn btn-primary" name="login">Bejelentkezés</button>
   </CENTER>
 </form>
