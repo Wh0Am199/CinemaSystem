47947375d15cdc86fab2f5c41ef9c5d3c1b32910
Mrk-PTi <markszalai22@gmail.com>
2020-04-07 15:34:48 +0200

    Login kódrész beillesztve a login.php -ba

--- PHP/protected/user/login.php
+++ PHP/protected/user/login.php
@@ -1,3 +1,12 @@
+<?php 
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+  $postData = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password']
+  ];
+?>
+
+
 <form method="post">
   <CENTER>
    <div class="form-group row">
