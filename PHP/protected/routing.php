7654e663890aeb752ba15df57f51f9ffa6f69f7f
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2020-04-15 12:57:23 +0200

    Szerkeszt
    Itt is hozzá adtam a szerkesztés.php-t a routing.php fájlba

--- PHP/protected/routing.php
+++ PHP/protected/routing.php
@@ -6,6 +6,7 @@ switch ($_GET['P']) {
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
 	case 'listaz': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/listaz.php' : header('Location: index.php'); break;
+	case 'szerkeszt': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/szerkeszt.php' : header('Location: index.php'); break;
 	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
 	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
