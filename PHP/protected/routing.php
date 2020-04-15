906fc3692ce29cbd723c64cb3da7e1314e62aaca
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2020-04-15 12:37:35 +0200

    Listázás
    Hozzá adtam a listázás opciót a routing.php-ba

--- PHP/protected/routing.php
+++ PHP/protected/routing.php
@@ -5,6 +5,7 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 switch ($_GET['P']) {
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
+	case 'listaz': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/listaz.php' : header('Location: index.php'); break;
 	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
 	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
