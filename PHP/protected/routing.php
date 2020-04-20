25bf16ca59a416619a0aed5c45f9617640a827e0
Wh0Am1 <balintb99@gmail.com>
2020-04-20 17:02:08 +0200

    Bug fix a routing.php-ban

--- PHP/protected/routing.php
+++ PHP/protected/routing.php
@@ -3,9 +3,10 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 	$_GET['P'] = 'home';
 
 switch ($_GET['P']) {
+	
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
-	case 'listaz': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/listaz.php' : header('Location: index.php'); break;
+	case 'listaz': !IsUserLoggedIn(); require_once PROTECTED_DIR.'user/listaz.php'; break;
 	case 'szerkeszt': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/szerkeszt.php' : header('Location: index.php'); break;
 	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
 	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
