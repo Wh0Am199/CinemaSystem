c79ad987b18d235135b591c2758aaaec4d183fce
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2020-04-22 13:06:45 +0200

    Szerkeszt
    Hozzá adtam a szerkesztés opciót a routing.php-ban

--- PHP/protected/routing.php
+++ PHP/protected/routing.php
@@ -6,8 +6,8 @@ switch ($_GET['P']) {
 	
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
-	case 'listaz': !IsUserLoggedIn(); require_once PROTECTED_DIR.'user/listaz.php'; break;
-	case 'szerkeszt': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/szerkeszt.php' : header('Location: index.php'); break;
+	case 'listaz': !IsUserLoggedIn(); require_once PROTECTED_DIR.'user/listaz.php'; break;	
+	case 'szerkeszt': !IsUserLoggedIn() ; require_once PROTECTED_DIR.'user/szerkeszt.php'; break;
 	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
 	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
