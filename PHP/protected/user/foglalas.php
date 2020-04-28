46c8d164001763a9a9db6a56e520e1887cfa6b2a
Mrk-PTi <markszalai22@gmail.com>
2020-04-28 21:10:59 +0200

    Form kiegészítés történt a foglalas.php -ban

--- PHP/protected/user/foglalas.php
+++ PHP/protected/user/foglalas.php
@@ -20,3 +20,36 @@
 		</select>
     </div>
 </div>
+<div class="form-group row">
+	<label for="date" class="col-sm-1 col-form-label" >Vetítések dátuma</label>
+	<div class="col-sm-3">
+		<select name="date" class="form-control">
+			<option value="2020-Jun-4">2020. Jún. 4.</option>
+			<option value="2020-Jun-6">2020. Jún. 6.</option>
+			<option value="2020-Jun-12">2020. Jún. 12.</option>
+			<option value="2020-Jun-19">2020. Jún. 19.</option>
+			<option value="2020-Jun-25">2020. Jún. 25.</option>
+			<option value="2020-Jun-30">2020. Jún. 30.</option>
+		</select>
+    </div>
+</div>
+<div class="form-group row">
+	<label for="show_time" class="col-sm-1 col-form-label" >Vetítés időpontja</label>
+	<div class="col-sm-3">
+		<select name="show_time" class="form-control">
+			<option value="10:00">10:00</option>
+			<option value="13:00">13:00</option>
+			<option value="17:00">17:00</option>
+			<option value="21:00">21:00</option>
+		</select>
+    </div>
+</div>
+<div class="form-group row">
+	<label for="seats" class="col-sm-1 col-form-label" >Ülőhelyek száma</label>
+	<div class="col-sm-3">
+		<input type="number" name="seats" min="1" max="4" value="1">
+    </div>
+</div>
+</CENTER>
+<button type="submit" class="btn btn-primary" name="lefoglal">Lefoglal</button>
+</form>
