c6fa26246d118ffa9aa124132e82d5add8056054
Mrk-PTi <markszalai22@gmail.com>
2020-04-29 12:14:44 +0200

    routing.php kiegészítve a foglalas átírányítással

--- PHP/protected/routing.php
+++ PHP/protected/routing.php
@@ -8,6 +8,7 @@ switch ($_GET['P']) {
 	case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
 	case 'listaz': !IsUserLoggedIn(); require_once PROTECTED_DIR.'user/listaz.php'; break;	
 	case 'szerkeszt': !IsUserLoggedIn() ; require_once PROTECTED_DIR.'user/szerkeszt.php'; break;
+	case 'foglalas': !IsUserLoggedIn(); require_once PROTECTED_DIR.'user/foglalas.php'; break;
 	case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
 	case 'logout': IsUserLoggedIn() ? UserLogout() : header('Location: index.php'); break;
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
