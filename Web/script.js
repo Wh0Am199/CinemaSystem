4f4535933a24380cc4ca7b5ac787e854226efd02
DBotond <dbotond89@gmail.com>
2020-04-29 11:46:48 +0200

    Új sor metódus váza hozzáadva a script.js-hez

--- Web/script.js
+++ Web/script.js
@@ -1,1 +1,17 @@
-//még üres
+function addField (argument) {
+        var myTable = document.getElementById("myTable");
+        var currentIndex = myTable.rows.length;
+        var currentRow = myTable.insertRow(-1);		       
+		
+        var currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(indexfield);
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(usernamefield);
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(emailbox);
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(deletebox);
+}
